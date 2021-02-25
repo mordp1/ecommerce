78f4b9ba66c97070c49a723e9babdd46b98e0e46
mordp1 <marcos.ordp@gmail.com>
2021-02-25 17:21:19 -0300

    Simplificado NewOrderMain e Criado KafkaDispatcher

---
+++ src/main/java/mordp/com/KafkaDispatcher.java
@@ -0,0 +1,41 @@
+package mordp.com;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+class KafkaDispatcher {
+
+    private final KafkaProducer<String, String> producer;
+
+    KafkaDispatcher(){
+       this.producer = new KafkaProducer<>(properties())
+
+    }
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty (ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "129.213.117.48:9092");
+        properties.setProperty (ProducerConfig.CLIENT_DNS_LOOKUP_CONFIG, "use_all_dns_ips");
+        properties.setProperty (ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty (ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+
+    void send(String ecommerce_new_order, String key, String value) throws ExecutionException, InterruptedException {
+        var record = new ProducerRecord<>(, key, value);
+        Callback getCallback()
+            return (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println("Sucesso enviando " + data.topic() + ":::Partition " + data.partition() + "/ Offset " + data.offset() + "/ Timestamp " + data.timestamp());
+            };
+        producer.send(record, getCallback).get();
+    }
+}
--- src/main/java/mordp/com/NewOrderMain.java
+++ src/main/java/mordp/com/NewOrderMain.java
@@ -1,46 +1,17 @@
 package mordp.com;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.net.InetAddress;
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
+        var dispatcher = new KafkaDispatcher();
         for (var i = 0; i < 10; i++) {
             var key = UUID.randomUUID().toString();
-            var value = key + ",555,33333";
-            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
-            var email = "Obrigado pela sua Order, Estamos processando";
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_NEW_EMAIL", key, email);
-            producer.send(record, getCallback()).get();
-            producer.send(emailRecord, getCallback()).get();
-        }
-    }
-    private static Callback getCallback() {
-        return (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("Sucesso enviando " + data.topic() + ":::Partition " + data.partition() + "/ Offset " + data.offset() + "/ Timestamp " + data.timestamp());
-        };
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty (ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "129.213.117.48:9092");
-        properties.setProperty (ProducerConfig.CLIENT_DNS_LOOKUP_CONFIG, "use_all_dns_ips");
-        properties.setProperty (ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty (ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+            var value = key + ",555,33333,123";
+            dispatcher.send ("ECOMMERCE_NEW_ORDER", key, value);
+            var email = "Obrigado pelsa sua Order, Estamos processando"
+            dispatcher.send("ECCOMERCE_NEW_EMAIL",key,email);
     }
 }
