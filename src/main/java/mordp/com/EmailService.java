ab23cf06bf934c9ee372a0f5f642207cee2670f3
mordp1 <marcos.ordp@gmail.com>
2021-02-25 15:49:45 -0300

    Criação do KafkaService

--- src/main/java/mordp/com/EmailService.java
+++ src/main/java/mordp/com/EmailService.java
@@ -1,6 +1,7 @@
 package mordp.com;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -11,41 +12,26 @@ import java.util.Properties;
 public class EmailService {
 
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_EMAIL"));
-        while (true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                System.out.println("Encontrei " + records.count() +  " registros");
-                for (var record : records) {
-                    System.out.println("Enviando EMAIL");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // ignore
-                        e.printStackTrace();
-                    }
-                    System.out.println("Email Enviado");
-                }
-            }
+        var emailService = new EmailService();
+        var service = new KafkaService("ECOMMERCE_NEW_EMAIL",
+                emailService::parse);
+        service.run();
+    }
 
+    private void parse(ConsumerRecord<String,String> record) {
+        System.out.println("Enviando EMAIL");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // ignore
+            e.printStackTrace();
         }
+        System.out.println("Email Enviado");
+
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "129.213.117.48:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-
-
-
-        return properties;
     }
-
-}
