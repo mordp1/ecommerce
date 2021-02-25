cf30602455e1ffdee805c48e2b887e4c8807fc54
mordp1 <marcos.ordp@gmail.com>
2021-02-25 10:42:00 -0300

    Log Service

--- src/main/java/mordp/com/LogService.java
+++ src/main/java/mordp/com/LogService.java
@@ -7,29 +7,23 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 public class LogService {
 
     public static void main(String[] args) {
         var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
         while (true) {
             var records = consumer.poll(Duration.ofMillis(100));
             if (!records.isEmpty()) {
                 System.out.println("Encontrei " + records.count() +  " registros");
                 for (var record : records) {
-                    System.out.println("Processando New Order, verificando fruade");
+                    System.out.println("LOG:" + record.topic());
                     System.out.println(record.key());
                     System.out.println(record.value());
                     System.out.println(record.partition());
                     System.out.println(record.offset());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        // ignore
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processada");
                 }
             }
 
