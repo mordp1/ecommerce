195535b55a364c60a560e030aba7ae366dfbe9c2
mordp1 <marcos.ordp@gmail.com>
2021-02-25 09:56:33 -0300

    v1

---
+++ src/main/java/mordp/com/NewOrderMain.java
@@ -0,0 +1,35 @@
+package mordp.com;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.InetAddress;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "123,436,6821";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER",value, value );
+        producer.send(record,(data, ex) -> {
+            if (ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("Sucesso enviando " + data.topic() + ":::Partition " + data.partition() + "/ Offset " + data.offset() + "/ Timestamp " + data.timestamp());
+        }).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty (ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "129.213.117.48:9092");
+        properties.setProperty (ProducerConfig.CLIENT_DNS_LOOKUP_CONFIG, "use_all_dns_ips");
+        properties.setProperty (ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty (ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
