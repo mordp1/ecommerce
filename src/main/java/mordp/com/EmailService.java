a9e7a96cc272855e0c9e7070dd43d6c6600abee5
mordp1 <marcos.ordp@gmail.com>
2021-03-06 17:24:54 -0300

    Serviços Ok, todos funcionando corretamente

--- src/main/java/mordp/com/EmailService.java
+++ src/main/java/mordp/com/EmailService.java
@@ -2,6 +2,8 @@ package mordp.com;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.Map;
+
 public class EmailService {
 
     public static void main(String[] args) {
@@ -9,17 +11,19 @@ public class EmailService {
         try (var service = new KafkaService (EmailService.class.getSimpleName(),
                 "ECOMMERCE_NEW_EMAIL",
                 emailService::parse,
-                Order.class)) {
+                String.class,
+                Map.of())) {
             service.run();
         }
     }
 
     private void parse(ConsumerRecord<String,String> record) {
-        System.out.println("Enviando EMAIL");
-        System.out.println(record.key());
-        System.out.println(record.value());
-        System.out.println(record.partition());
-        System.out.println(record.offset());
+        System.out.println("---------------------------------------------------");
+        System.out.println("Enviando EMAIL ");
+        System.out.println("Key: " + record.key());
+        System.out.println("Value: " + record.value());
+        System.out.println("Partition: " + record.partition());
+        System.out.println("Offset: " + record.offset());
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
--- src/main/java/mordp/com/FraudDetectorService.java
+++ src/main/java/mordp/com/FraudDetectorService.java
@@ -1,6 +1,9 @@
 package mordp.com;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FraudDetectorService {
 
     public static void main(String[] args) {
@@ -8,18 +11,20 @@ public class FraudDetectorService {
         try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
                 "ECOMMERCE_NEW_ORDER",
                 fraudService::parse,
-                Order.class)) {
+                Order.class,
+                Map.of())) {
             service.run();
         }
 
     }
 
     private void parse(ConsumerRecord<String, Order> record) {
+        System.out.println("-------------------------------------------------");
         System.out.println("Processando New Order, verificando fruade");
-        System.out.println(record.key());
-        System.out.println(record.value());
-        System.out.println(record.partition());
-        System.out.println(record.offset());
+        System.out.println("Key: " + record.key());
+        System.out.println("Value: " + record.value());
+        System.out.println("Partiton: " + record.partition());
+        System.out.println("Offset: " + record.offset());
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
