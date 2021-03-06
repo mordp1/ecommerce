fe1c848e9a3d36377a4a49f31e943e73632c6312
mordp1 <marcos.ordp@gmail.com>
2021-03-06 18:05:55 -0300

    Serviços Ok, todos funcionando corretamente

---
+++ service-new-order/src/main/java/mordp/com/Order.java
@@ -0,0 +1,15 @@
+package mordp.com;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+    private final String userID, orderID;
+    private final BigDecimal amount;
+
+    public Order(String userID, String orderID, BigDecimal amount) {
+        this.userID = userID;
+        this.orderID = orderID;
+        this.amount = amount;
+    }
+}
