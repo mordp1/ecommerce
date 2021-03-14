6be7aa6a397e8af74ce6d00fc4efd3a7c93405a9
mordp1 <marcos.ordp@gmail.com>
2021-03-14 14:14:09 -0300

    service redding report - simulação de geração de relatórios em disco

--- service-reding-report/src/main/java/mordp/com/ReadingReportService.java
+++ service-reding-report/src/main/java/mordp/com/ReadingReportService.java
@@ -1,51 +1,37 @@
 package mordp.com;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class ReadingReportService {
 
+    private static final Path SOURCE = new File("src/main/resources/reports.txt").toPath();
+
     public static void main(String[] args) {
-        var fraudService = new ReadingReportService();
+        var reportService = new ReadingReportService();
         try (var service = new KafkaService<>(ReadingReportService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                fraudService::parse,
-                Order.class,
+                "USER_GENERATE_READING_REPORT",
+                reportService::parse,
+                User.class,
                 Map.of())) {
             service.run();
         }
     }
 
-    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
-
-    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
+    private void parse(ConsumerRecord<String,User> record) throws IOException {
         System.out.println("-------------------------------------------------");
-        System.out.println("Processando New Order, verificando fruade");
-        System.out.println("Key: " + record.key());
-        System.out.println("Value: " + record.value());
-        System.out.println("Partiton: " + record.partition());
-        System.out.println("Offset: " + record.offset());
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            // ignore
-            e.printStackTrace();
-        }
-        var order = record.value();
-        if (isFraud(order)) {
-            // pretending that the fraud happens when the amount is >= 4500
-            System.out.println("Order is Fraud!!! " +order);
-            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getEmail(), order);
-        } else {
-            System.out.println("Approved: " + order);
-            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getEmail(), order);
-        }
+        System.out.println("Processing report for " + record.value());
+        var user = record.value();
+        var target = new File(user.getReportPath());
+        IO.copyTo(SOURCE, target);
+        IO.append(target, "Created for " + user.getUuid());
+        System.out.println("File Created: " + target.getAbsolutePath());
 
-    }
 
-    private boolean isFraud(Order order) {
-        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
     }
 }
